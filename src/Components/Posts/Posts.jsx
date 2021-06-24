30cdafcb8e3e4f5182c3fb6d1d537e8454dc0285
Erick Lumunge <lumungep12@gmail.com>
2021-06-24 07:01:30 +0300

    post

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
-import { Grid, CircularProgress } from '@material-ui/core';
+import React, {useState, useEffect} from 'react';
+import { Grid, CircularProgress, Button } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
 import useStyles from './styles';
 
 const Posts = ({ setCurrentId }) => {
+  const [filteredPosts, setFilteredPosts] = useState([]);
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
+
+  const filterByCategory = (category) => {
+    const newPosts = posts.filter((post) => post.category === category);
+    setFilteredPosts(newPosts);
+  }
+
   return (
     !posts.length ? <CircularProgress /> : (
+      <>
+      <div>
+        {posts.map((post) => (
+          <Button variant="contained" onClick={() => filterByCategory(post.category)}>{post.category}</Button>
+        ))}
+      </div>
+      <br /><br />
       <Grid className={classes.container} container alignItems="stretch" spacing={3}>
         {posts.map((post) => (
           <Grid key={post._id} item xs={12} sm={6} md={6}>
@@ -18,6 +32,7 @@ const Posts = ({ setCurrentId }) => {
           </Grid>
         ))}
       </Grid>
+      </>
     )
   );
 };
